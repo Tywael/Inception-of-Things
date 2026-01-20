be1c207f0f4d6dff5028622265514ca51039ecbe
Tywael <yves.althaus42@gmail.com>
2026-01-20 16:15:38 +0000

    fix p2 real default app3

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -8,18 +8,20 @@ sudo apt update -y
 sudo apt install -y net-tools
 
 /usr/local/bin/kubectl create configmap app1-html \
-  --from-file=index.html=/pods-cfg/app1/index.html \
+  --from-file=index.html=/confs/app1/index.html \
   --dry-run=client -o yaml | /usr/local/bin/kubectl apply -f -
-/usr/local/bin/kubectl apply -f /pods-cfg/app1/app1.yaml
+/usr/local/bin/kubectl apply -f /confs/app1/app1.yaml
 
 
 /usr/local/bin/kubectl create configmap app2-html \
-  --from-file=index.html=/pods-cfg/app2/index.html \
+  --from-file=index.html=/confs/app2/index.html \
   --dry-run=client -o yaml | /usr/local/bin/kubectl apply -f -
-/usr/local/bin/kubectl apply -f /pods-cfg/app2/app2.yaml
+/usr/local/bin/kubectl apply -f /confs/app2/app2.yaml
 
 
 /usr/local/bin/kubectl create configmap app3-html \
-  --from-file=index.html=/pods-cfg/app3/index.html \
+  --from-file=index.html=/confs/app3/index.html \
   --dry-run=client -o yaml | /usr/local/bin/kubectl apply -f -
-/usr/local/bin/kubectl apply -f /pods-cfg/app3/app3.yaml
+/usr/local/bin/kubectl apply -f /confs/app3/app3.yaml
+
+kubectl apply -f /confs/ingress.yaml
